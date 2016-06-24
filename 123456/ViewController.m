b327f2188349ba48e915d474ca4e94714f6278f4
LWXGithubTest <236181999@qq.com>
2016-06-24 19:56:53 +0800

    qiguohui
    add top

---
+++ 123456/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  123456
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 qiguohui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
